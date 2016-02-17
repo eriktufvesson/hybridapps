11c43d324a6c0573a96b33c595318e463737d370
Erik Tufvesson <erik.tufvesson@gmail.com>
2016-02-17 11:07:49 +0100

    slides and mobile webb

--- examples/ionic2/ShopDemo/app/config.js
+++ examples/ionic2/ShopDemo/app/config.js
@@ -1,2 +1,2 @@
-// export var API_URL = 'https://shopdemoapi.herokuapp.com/';
-export var API_URL = 'http://localhost:3000/';
+export var API_URL = 'https://shopdemoapi.herokuapp.com/';
+//export var API_URL = 'http://localhost:3000/';
