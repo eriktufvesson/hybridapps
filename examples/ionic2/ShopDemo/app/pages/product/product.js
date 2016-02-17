084a1f6bd298223b4d04139a0b42344fab161ae2
Presis <presis@Andreass-MacBook-Pro.local>
2016-02-17 16:38:58 +0100

    shopdemo comments

--- examples/ionic2/ShopDemo/app/pages/product/product.js
+++ examples/ionic2/ShopDemo/app/pages/product/product.js
@@ -1,60 +1,100 @@
-import {Page, NavController, NavParams, Modal} from 'ionic/ionic';
+import {Page, NavController, NavParams, Modal, ViewController} from 'ionic/ionic';
 import {NavbarButtons} from '../../components/navbar-buttons/navbar-buttons';
 import {AddToCartModal} from '../cart/cart';
 import {CartPage} from '../cart/cart';
 import {Products} from '../../providers/products/products';
 
 @Page({
-  templateUrl: 'build/pages/product/product.html',
-  directives: [NavbarButtons]
+    templateUrl: 'build/pages/product/product.html',
+    directives: [NavbarButtons]
 })
 export class ProductPage {
-  constructor(nav: NavController, params: NavParams, productService: Products) {
-    this.nav = nav;
-    this.params = params;
-    this.productService = productService;
-    
-    this.product = params.get("product");
-    this.userId = 1;
-  }
-  
-  ngOnInit() {
-    this.productService.likes(this.product.id).subscribe((likes) => {
-      this.likes = likes;
-      this.product.nbrLikes = likes.length;
-    });
-    this.productService.comments(this.product.id).subscribe((comments) => {
-      this.comments = comments;
-      this.product.nbrComments = comments.length;
-    });
-  }
-  
-  addToCart(product) {
-    let addToCartModal = Modal.create(AddToCartModal, { product: product });
-    addToCartModal.onDismiss(data => {
-      console.log(data);
-    });
-    this.nav.present(addToCartModal); 
-  }
-  
-  hasLiked(product) {
-    if (!this.likes) return false;
-    return this.likes.filter((like) => like.userId = this.userId).length;
-  }
-  
-  like(product) {
-    if (this.hasLiked(product)) {
-      return;
+    constructor(nav: NavController, params: NavParams, productService: Products) {
+        this.nav = nav;
+        this.params = params;
+        this.productService = productService;
+
+        this.product = params.get("product");
+        this.userId = 1;
     }
-    this.productService.addLike(product, this.userId).subscribe(
-      like => {
-        console.log("Product is liked!");
-        this.likes.push(like);
-        product.nbrLikes++;
-      });
-  }
-  
-  navCart() {
-    this.nav.push(CartPage);
-  }
+
+    ngOnInit() {
+        this.productService.likes(this.product.id).subscribe((likes) => {
+            this.likes = likes;
+            this.product.nbrLikes = likes.length;
+        });
+        this.productService.comments(this.product.id).subscribe((comments) => {
+            this.comments = comments;
+            this.product.nbrComments = comments.length;
+        });
+    }
+
+    addToCart(product) {
+        let addToCartModal = Modal.create(AddToCartModal, { product: product });
+        addToCartModal.onDismiss(data => {
+            console.log(data);
+        });
+        this.nav.present(addToCartModal);
+    }
+
+    hasLiked(product) {
+        if (!this.likes) return false;
+        return this.likes.filter((like) => like.userId = this.userId).length;
+    }
+
+    like(product) {
+        if (this.hasLiked(product)) {
+            return;
+        }
+        this.productService.addLike(product, this.userId).subscribe(
+            like => {
+                console.log("Product is liked!");
+                this.likes.push(like);
+                product.nbrLikes++;
+            });
+    }
+
+    openComments(product, comments) {
+        let productCommentsModal = Modal.create(ProductCommentsModal, { product, comments });
+        productCommentsModal.onDismiss(data => {
+            console.log(data);
+        });
+        this.nav.present(productCommentsModal);
+    }
+
+    navCart() {
+        this.nav.push(CartPage);
+    }
+}
+
+@Page({
+    templateUrl: 'build/pages/product/product-comments.html'
+})
+export class ProductCommentsModal {
+    constructor(viewCtrl: ViewController, navParams: NavParams, nav: NavController, productService: Products) {
+        this.viewCtrl = viewCtrl;
+        this.product = navParams.get('product');
+        this.comments = navParams.get('comments');
+        this.nav = nav;
+        this.productService = productService;
+        this.commentBody = '';
+    }
+
+    addComment() {
+        if (this.commentBody) {
+            this.productService.addComment({
+                userId: 1,
+                body: this.commentBody,
+                productId: this.product.id
+            }).subscribe(comment => {
+                this.comments.push(comment);
+                this.commentBody = '';
+            });
+        }
+    }
+
+    cancel() {
+        this.viewCtrl.dismiss();
+    }
+
 }
--- examples/ionic2/ShopDemo/app/providers/products/products.js
+++ examples/ionic2/ShopDemo/app/providers/products/products.js
@@ -6,43 +6,52 @@ import {API_URL} from '../../config';
 
 @Injectable()
 export class Products {
-  constructor(http: Http) {
-    this.http = http;
+    constructor(http: Http) {
+        this.http = http;
 
-    this.products = null;
-  }
+        this.products = null;
+    }
 
-  all() {
-    return this.http.get(API_URL + 'products')
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
+    all() {
+        return this.http.get(API_URL + 'products')
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
 
-  likes(productId) {
-    return this.http.get(API_URL + 'likes?productId=' + productId)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-  
-  addLike(product, userId) {
-    let like = { productId: product.id, userId: userId };
-    let body = JSON.stringify(like);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({headers: headers});
-    return this.http.post(API_URL + 'likes', body, options)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-  
-  comments(productId) {
-    return this.http.get(API_URL + 'comments?productId=' + productId)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
+    likes(productId) {
+        return this.http.get(API_URL + 'likes?productId=' + productId)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
 
-  handleError(error) {
-    console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
-  }
+    addLike(product, userId) {
+        let like = { productId: product.id, userId: userId };
+        let body = JSON.stringify(like);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(API_URL + 'likes', body, options)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    comments(productId) {
+        return this.http.get(API_URL + 'comments?productId=' + productId)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    addComment(comment) {
+        let body = JSON.stringify(comment);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(API_URL + 'comments', body, options)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    handleError(error) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
 }
 
